7bce5f65b679bb29eec2cd654a31110cc18fb76b
Justin S. Baiko <baiko@utexas.edu>
2017-02-08 00:51:34 -0600

    laying the blue print

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("hello there!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import StickyNote from './components/stickyNote'
+
+
+class App extends React.Component {
+   constructor() {
+     super();
+     this.state = {
+       todos: ["finish my project","get some sleep","eat healthy","work out"],
+       todo: ""
+     }
+     this.addTask = this.addTask.bind(this);
+     this.handleTask = this.handleTask.bind(this);
+   }
+   addTask(newTask) {
+     this.setState({
+        todo: newTask.target.value
+     });
+   }
+   handleTask() {
+      var task = this.state.todo;
+      var list = this.state.todos;
+      list.push(task);
+      this.setState({
+        todos: list
+      });
+   }
+   render() {
+     return (<div>
+       <h1>Todo List</h1>
+       <ol>
+         {this.state.todos.map(
+           (todo,index) => <StickyNote key={index} task={todo} />
+         )}
+       </ol>
+       <input value={this.state.todo} onChange={this.addTask} type="text" />
+       <button onClick={this.handleTask}>Add Todo</button>
+     </div>
+    );
+   }
+}
+
+
+ReactDOM.render(<App />,document.getElementById('app'));
