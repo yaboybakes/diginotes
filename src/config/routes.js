92d1dacd07f1f2c9f2a0d0eaf8e6bac0ce9e48f8
Justin S. Baiko <baiko@utexas.edu>
2017-02-09 17:07:18 -0600

    added button css hover on notes and tweaked css

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {Router,Route,browserHistory,IndexRoute} from 'react-router'
 import BulletinBoard from '../components/BulletinBoard'
 
-
 module.exports = (
       <Router history={browserHistory}>
         <Route path='/' component={BulletinBoard}>
